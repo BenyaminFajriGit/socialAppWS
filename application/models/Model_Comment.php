827574a2b477df989300ba19be3552371d6c8654
rifkisrg <45476728+rifkisrg@users.noreply.github.com>
2019-10-27 22:41:15 +0700

    Update Model_Comment.php
    Ubah nama class menjadi Komentar, dan mengubah keseluruhan isi class.

--- application/models/Model_Comment.php
+++ application/models/Model_Comment.php
@@ -1,33 +1,55 @@
 <?php
 defined ('BASEPATH') OR exit ('No direct script access allowed');
-class Model_Comment extends CI_Model
-{
-    private $_table = "comment";
+class Komentar extends CI_Model{
+    public function __construct(){
+        $this->load->database();
+        $this->load->model('Komentar');
+    }
 
-    public $id;
-    public $username;
-    public $id_post;
-    public $caption;
-    public $tanggal;
+    public function setComment($data){
+        $query = $this->db->insert('comment', $data);
 
+        if($query){
+            $komentarBerita = $this->getPostComment($data['id_post']);
 
-     public function getByIdPost($id_post)
-     {
-          return $this->db->get_where($this->_table, ["id_post" => $id_post])->result();
-     }
+            $res['status'] = true;
+            $res['message'] = 'Data Berhasil Ditambahkan';
+            $res['data'] = $komentarBerita;
+        } else {
+            $res['status'] = false;
+            $res['message'] = 'Data Tidak Berhasil Ditambahkan';
+        }
 
-     public function add($username,$id_post,$caption){
-          $this->username=$username;
-          $this->id_post=$id_post;
-          $this->caption=$caption;
-          $this->tanggal= date("Y-m-d H:i:s");
-       return $this->db->insert($this->_table, $this);
-     }
+        return res;
+    }
 
-     public function delete($id){
-          return $this->db->delete($this->_table,  array("id" => $id));
-     }
+    public function getPostComment($idPost){
+        $query = $this->db->get('comment')->where('id_post', $idPost);
+        $result = $query->result();
 
+        if (empty($result) || is_null($result)) {
+            $res['status'] = false;
+            $res['message'] = 'Data tidak Ditemukan';
+        } else {
+            $res['status'] = true;
+            $res['message'] = 'Data Ditemukan';
+            $res['data'] = $result;
+        }
+        return $res;
 
-   
+    }
+
+    public function deleteComment($idComment){
+        $query = $this->db->delete('comment', array('id_comment' => $idComment));
+
+        if ($query) {
+            $res['status'] = true;
+            $res['message'] = 'Data berhasil dihapus';
+        } else {
+            $res['status'] = false;
+            $res['message'] = 'Data tidak berhasil dihapus';
+        }
+
+        return $res;
+    }
 }
