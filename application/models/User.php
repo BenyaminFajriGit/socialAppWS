6ea1af6c5fb7b6352cbced9a27527fbe0e4a828e
rifkisrg <45476728+rifkisrg@users.noreply.github.com>
2019-10-30 19:59:06 +0700

    Update User.php
    nambahin validate login dan user update

--- application/models/User.php
+++ application/models/User.php
@@ -7,21 +7,55 @@ class User extends CI_Model{
     }
 
     public function addUser($dataUser){
-        $query = $this->db->insert('users', $dataUser);
+        if ($this->cekUser($dataUser['username'])) {
+            $query = $this->db->insert('user', $dataUser);
 
-        if ($query) {
-            $res['status'] = true;
-            $res['message'] = 'Data Berhasil Ditambahkan';
-        } else {
+            if($query){
+                $res['status'] = true;
+                $res['message'] = 'Data Berhasil Ditambahkan';
+            }else{
+                $res['status'] = false;
+                $res['message'] = 'Data Tidak Berhasil Ditambahkan';
+            }
+        }
+        else {
             $res['status'] = false;
-            $res['message'] = 'Data Tidak Berhasil Ditambahkan';
+            $res['message'] = 'Data Sudah Ada';
+        }
+
+        return $res;
+    }
+
+    public function cekUser($username){
+        $query = $this->db->where('username', $username)->get('user');
+
+        if($query->num_rows() == 0){
+            $status = true;
+        }else{
+            $status = false;
+        }
+
+        return $status;
+    }
+
+    public function validateLogin($data){
+        $query = $this->db->where($data);
+        $result = $query->get('user')->result();
+
+        if (empty($result) || is_null($result)) {
+            $res['status'] = false;
+            $res['message'] = 'Data tidak Ditemukan';
+        } else {
+            $res['status'] = true;
+            $res['message'] = 'Data Ditemukan';
+            $res['data'] = $result;
         }
 
         return $res;
     }
 
     public function userAll(){
-        $query = $this->db->get("users");
+        $query = $this->db->get("user");
         $result = $query->result();
 
         if (empty($result) || is_null($result)) {
@@ -35,9 +69,23 @@ class User extends CI_Model{
         return $res;
     }
 
-    public function getDataUser($idUser){
-        $query = $this->db->where("id", $idUser);
-        $result = $query->get('users')->result();
+    public function updateUser($idUser, $newData){
+        $query = $this->db->where('id_user', $idUser)->update('user', $newData);
+
+        if ($query) {
+            $res['status'] = true;
+            $res['message'] = 'Data Berhasil Diubah';
+        } else {
+            $res['status'] = false;
+            $res['message'] = 'Data Tidak Berhasil Diubah';
+        }
+
+        return $res;
+    }
+
+    public function getDataUser($username){
+        $query = $this->db->where("username", $username);
+        $result = $query->get('user')->result();
 
         if (empty($result) || is_null($result)) {
             $res['status'] = false;
@@ -50,8 +98,8 @@ class User extends CI_Model{
         return $res;
     }
 
-    public function deleteUser($idUser){
-        $query = $this->db->delete('users', array('id', $idUser));
+    public function deleteUser($username){
+        $query = $this->db->delete('user', array('username' => $username));
 
         if ($query) {
             $res['status'] = true;
