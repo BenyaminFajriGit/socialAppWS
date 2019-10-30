c6d0b3cc995b27edefc37f50ea2c053ead16a864
rifkisrg <45476728+rifkisrg@users.noreply.github.com>
2019-10-30 19:57:08 +0700

    Update DataUser.php

--- application/controllers/bak/DataUser.php
+++ application/controllers/bak/DataUser.php
@@ -9,14 +9,38 @@ class DataUser extends CI_Controller{
 
     public function addUser(){
         $data = array(
-            'id' => 0,
-            'nama' => $this->input->post('nama'),
-            'username' => $this->input->post('username'),
-            'password' => $this->input->post('password'),
-            'no_hp' => $this->input->post('no_hp')
+            'nama' => $this->input->get('nama'),
+            'username' => $this->input->get('username'),
+            'password' => md5($this->input->get('password')),
+            'no_hp' => $this->input->get('no_hp')
         );
 
         $res = $this->User->addUser($data);
+
+        echo json_encode($res);
+    }
+
+    public function validateLogin(){
+        $data = array(
+            'username' => $this->input->get('username'),
+            'password' => md5($this->input->get('password'))
+        );
+
+        $res = $this->User->validateLogin($data);
+
+        echo json_encode($res);
+    }
+
+    public function updateData(){
+        $idUser = $this->input->get('id_user');
+        $newData = array(
+            'username' => $this->input->get('username'),
+            'nama' => $this->input->get('nama'),
+            'password' => md5($this->input->get('password')),
+            'no_hp' => $this->input->get('no_hp')
+        );
+
+        $res = $this->User->updateUser($idUser, $newData);
 
         echo json_encode($res);
     }
@@ -28,15 +52,15 @@ class DataUser extends CI_Controller{
     }
 
     public function getDataUser(){
-        $idUser = $this->input->post('id_user');
-        $result = $this->User->getDataUser($idUser);
+        $username = $this->input->get('username');
+        $result = $this->User->getDataUser($username);
 
         echo json_encode($result);
     }
 
     public function deleteUser(){
-        $idUser = $this->input->post('id_user');
-        $result = $this->User->deleteUser($idUser);
+        $username = $this->input->get('username');
+        $result = $this->User->deleteUser($username);
 
         echo json_encode($result);
     }
