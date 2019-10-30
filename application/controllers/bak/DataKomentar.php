62527c9218fa3cadea0270162361814a8618be0b
rifkisrg <45476728+rifkisrg@users.noreply.github.com>
2019-10-30 19:55:35 +0700

    Update DataKomentar.php

--- application/controllers/bak/DataKomentar.php
+++ application/controllers/bak/DataKomentar.php
@@ -1,8 +1,10 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class DataKomentar extends CI_Controller{
     public function __construct()
     {
+		parent::__construct();
         $this->load->model('Komentar');
     }
 
